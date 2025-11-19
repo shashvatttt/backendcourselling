bf1f83fc41811eadfc470d153e31cb5e206ce285
shashvatttt <shashvatsingh459@gmail.com>
2025-11-19 09:03:44 +0530

    update

---
+++ db.js
@@ -0,0 +1,22 @@
+const {Schema, default: mongoose} = require("mongoose")
+
+const userSchema = Schema({
+    
+})
+
+const adminSchema = Schema({
+    
+})
+
+const courseSchema = Schema({
+    
+})
+
+const purchaseSchema = Schema({
+    
+})
+
+const userModel = mongoose.Model("user", userSchema)
+const adminModel = mongoose.Model("admin", userSchema)
+const courseModel = mongoose.Model("course", userSchema)
+const purchaseModel = mongoose.Model("purchase", userSchema)
